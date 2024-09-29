2f5236cc091592fe5a96342788c0702004f8b5fe
RizwanMunawar <chr043416@gmail.com>
2024-09-29 18:09:07 +0500

    update

---
+++ app.py
@@ -0,0 +1,44 @@
+import gradio as gr
+import PIL.Image as Image
+
+from ultralytics import ASSETS, YOLO
+
+model = None
+
+
+def predict_image(img, conf_threshold, iou_threshold, model_name):
+    """Predicts objects in an image using a YOLOv8 model with adjustable confidence and IOU thresholds."""
+    model = YOLO(model_name)
+    results = model.predict(
+        source=img,
+        conf=conf_threshold,
+        iou=iou_threshold,
+        show_labels=True,
+        show_conf=True,
+        imgsz=640,
+    )
+
+    for r in results:
+        im_array = r.plot()
+        im = Image.fromarray(im_array[..., ::-1])
+
+    return im
+
+
+iface = gr.Interface(
+    fn=predict_image,
+    inputs=[
+        gr.Image(type="pil", label="Upload Image"),
+        gr.Slider(minimum=0, maximum=1, value=0.25, label="Confidence threshold"),
+        gr.Slider(minimum=0, maximum=1, value=0.45, label="IoU threshold"),
+        gr.Radio(choices=["yolov8n", "yolov8s", "yolov8m"], label="Model Name", value="yolov8n"),
+    ],
+    outputs=gr.Image(type="pil", label="Result"),
+    title="Ultralytics Gradio Application 🚀",
+    description="Upload images for inference. The Ultralytics YOLOv8n model is used by default.",
+    examples=[
+        [ASSETS / "bus.jpg", 0.25, 0.45, "yolov8n.pt"],
+        [ASSETS / "zidane.jpg", 0.25, 0.45, "yolov8n.pt"],
+    ],
+)
+iface.launch(share=True)
